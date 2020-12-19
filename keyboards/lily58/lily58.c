622aeecc4987ee209d5470ea10c28cc100c6e335
sam <sam@BOTNET>
2020-12-19 00:37:22 +0000

    get stuff up to date

--- keyboards/lily58/lily58.c
+++ keyboards/lily58/lily58.c
@@ -3,3 +3,7 @@
 bool process_record_kb(uint16_t keycode, keyrecord_t *record) {
   return process_record_user(keycode, record);
 }
+
+// void encoder_update_kb(uint8_t index, bool clockwise) {
+    // encoder_update_user(index, clockwise);
+// }
